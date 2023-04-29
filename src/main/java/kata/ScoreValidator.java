cd3914182dcfc2b34ab435baa4945aa97fb5e8a3
Jonnt94 <73747046+Jonnt94@users.noreply.github.com>
2023-04-29 21:19:47 +0100

    Add MaxLength Validation.

--- src/main/java/kata/ScoreValidator.java
+++ src/main/java/kata/ScoreValidator.java
@@ -5,10 +5,48 @@ import java.util.List;
 public class ScoreValidator {
 
     public boolean validate(List<Integer> scores) {
-        if(scores.size() > 20){
+
+//    public boolean validate(List<Integer> scores) {
+//        if(scores.size() >= 10 && scores.size() <= 20){
+//            return true;
+//        }
+//
+//        return false;
+//    }
+//    public boolean validate(List<Integer> scores) {
+//        if(scores.contains(0)) {
+//            return false;
+//        }
+//
+//        return true;
+//
+//    }
+//        public boolean validate(List<Integer> scores) {
+//            if (scores.get(0) == 10 && scores.get(1) != 0) {
+//                return false;
+//            }
+//
+//            return true;
+//        }
+//    public boolean validate(List<Integer> scores) {
+//    if (scores.get(0) == 10 && scores.get(1) == 0) {
+//        return true;
+//    }
+//
+//    return false;
+//}
+//    public boolean validate(List<Integer> scores){
+//        if (scores.get(0) == -1){
+//            return false;
+//        }
+//        return true;
+//    }
+
+
+        public boolean validate(List<Integer> scores){
+            if (scores.contains(10)){
+                return true;
+            }
             return false;
         }
-
-        return true;
     }
-}
--- src/test/java/kata/bowling/ScoreValidatorTest.java
+++ src/test/java/kata/bowling/ScoreValidatorTest.java
@@ -44,4 +44,49 @@ public class ScoreValidatorTest {
 
         assertTrue(validationResult);
     }
+    @Test
+    void shouldReturnTrueWhenListLengthGreaterThan10AndLessThan20(){
+        List<Integer> scores = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        ScoreValidator scoreValidator = new ScoreValidator();
+
+        boolean validationResults = scoreValidator.validate(scores);
+
+        assertTrue(validationResults);
+    }
+    @Test
+    void shouldReturnFalseWhenAttemptTwoIsNot0WhenAttemptOneIs10(){
+        List<Integer> scores = List.of(10,11,12,13,14,15,16,17,18,19,20);
+        ScoreValidator scoreValidator = new ScoreValidator();
+
+        boolean validationResult = scoreValidator.validate(scores);
+
+        assertFalse(validationResult);
+    }
+    @Test
+    void shouldReturnTrueWhenAttemptTwoIs0WhenAttemptOneIs10() {
+        List<Integer> scores = List.of(10, 0, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+        ScoreValidator scoreValidator = new ScoreValidator();
+
+        boolean validationResult = scoreValidator.validate(scores);
+
+        assertTrue(validationResult);
+    }
+    @Test
+    void shouldReturnFalseWhenNumberEnteredLessThanZero(){
+        List<Integer> scores = List.of(-1);
+        ScoreValidator scoreValidator = new ScoreValidator();
+
+        boolean validationResult = scoreValidator.validate(scores);
+
+        assertFalse(validationResult);
+    }
+    @Test
+    void shouldReturnFalseWhenGivenMoreThanTen10s(){
+        List<Integer> scores = List.of(10,10,10,10,10,10,10,10,10,10,10);
+        ScoreValidator scoreValidator = new ScoreValidator();
+
+        boolean validationResult = scoreValidator.validate(scores);
+
+        assertFalse(validationResult);
+    }
 }

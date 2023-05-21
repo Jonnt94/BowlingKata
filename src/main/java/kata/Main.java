ceab0473df90979f85e2d21549227122955babff
Jonnt94 <73747046+Jonnt94@users.noreply.github.com>
2023-05-21 19:47:12 +0100

    tidy up frame class

--- src/main/java/kata/Main.java
+++ src/main/java/kata/Main.java
@@ -6,16 +6,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Frame frame1 = new Frame();
-        int gameTotal = frame1.FrameScore(10,2);
 
-        Frame frame2 = new Frame();
-        gameTotal += frame2.FrameScore(5,5);
-
-        Frame frame3 = new Frame();
-        gameTotal += frame3.FrameScore(9,4);
-
-        System.out.println(gameTotal);
     }
 
 }
--- src/main/java/kata/bowling/Frame.java
+++ src/main/java/kata/bowling/Frame.java
@@ -1,23 +1,16 @@
 package kata.bowling;
 
 public class Frame {
-    int attempt1;
-    int attempt2;
+    private final int attempt1;
+    private final int attempt2;
 
-    public int FrameScore(int try1, int try2){
-        attempt1 = try1;
-        attempt2 = try2;
+    public Frame(int attempt1, int attempt2) {
+        this.attempt1 = attempt1;
+        this.attempt2 = attempt2;
 
-        if(attempt1 == 10){
-            attempt2 = 0;
-        }
+    }
 
-        int frameTotal = attempt1 + attempt2;
-
-        if (frameTotal > 10){
-            frameTotal = 10;
-        }
-
-        return frameTotal;
+    public int calculateScore(){
+        return attempt1 + attempt2;
     }
 }

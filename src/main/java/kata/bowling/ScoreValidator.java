3d0873f7cb2c06456637d6f47af898bbc9fbcd44
Jonnt94 <73747046+Jonnt94@users.noreply.github.com>
2023-06-03 22:11:05 +0100

    Wiring ScoreValidator into ScoreBoard, also introduced Mockito.

--- src/main/java/kata/bowling/ScoreValidator.java
+++ src/main/java/kata/bowling/ScoreValidator.java
@@ -1,7 +1,10 @@
 package kata.bowling;
 
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
 public class ScoreValidator {
 
     public boolean validate(List<Integer> scores) {
--- src/test/java/kata/bowling/ScoreBoardTest.java
+++ src/test/java/kata/bowling/ScoreBoardTest.java
@@ -1,28 +1,56 @@
 package kata.bowling;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {ScoreBoard.class})
 class ScoreBoardTest {
+
+    @Autowired
+    private ScoreBoard scoreBoard;
+    @MockBean
+    private ScoreValidator scoreValidator;
     @Test
     void shouldCalculateAndReturnScore35() {
+        when(scoreValidator.validate(any())).thenReturn(true);
         List<Integer> scores = List.of(10, 10, 10, 5);
 
-        ScoreBoard scoreBoard = new ScoreBoard();
         int totalScore = scoreBoard.calculateScore(scores);
 
+        verify(scoreValidator).validate(List.of(10, 10, 10, 5));
         assertEquals(35, totalScore);
     }
     @Test
     void shouldCalculateAndReturnScore40() {
+        when(scoreValidator.validate(any())).thenReturn(true);
         List<Integer> scores = List.of(10, 10, 10, 10);
 
-        ScoreBoard scoreBoard = new ScoreBoard();
         int totalScore = scoreBoard.calculateScore(scores);
 
+        verify(scoreValidator).validate(List.of(10, 10, 10, 10));
         assertEquals(40, totalScore);
+    }
+    @Test
+    void shouldReturn0WhenScoresNotValid() {
+        when(scoreValidator.validate(any())).thenReturn(false);
+        List<Integer> scores = List.of(10, 10, 10, 10);
+
+        int totalScore = scoreBoard.calculateScore(scores);
+
+        verify(scoreValidator).validate(List.of(10, 10, 10, 10));
+        assertEquals(0, totalScore);
     }
 }

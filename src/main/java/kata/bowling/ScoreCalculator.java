7cc302903207720682f1e76b2b6dedba51b334b6
Jonnt94 <73747046+Jonnt94@users.noreply.github.com>
2023-07-29 20:56:40 +0100

    Making ScoreCalculator more readable.

--- src/main/java/kata/bowling/ScoreCalculator.java
+++ src/main/java/kata/bowling/ScoreCalculator.java
@@ -36,22 +36,37 @@ public class ScoreCalculator {
             int totalPinsKnockedDownInFrame = frame.totalPinsKnockedDown();
             runningTotal += totalPinsKnockedDownInFrame;
             if (totalPinsKnockedDownInFrame == 10) {
-                Frame nextFrame = frames.get(i + 1);
-                if (frame.pinsKnockedDownAttempt1() == 10) {
-                    int attempt1BonusPins = nextFrame.pinsKnockedDownAttempt1();
-                    int attempt2BonusPins;
-                    if(attempt1BonusPins == 10){
-                        Frame nextNextFrame = frames.get(i + 2);
-                        attempt2BonusPins = nextNextFrame.pinsKnockedDownAttempt1();
-                    } else {
-                        attempt2BonusPins = nextFrame.pinsKnockedDownAttempt2();
-                    }
-                    runningTotal += attempt1BonusPins + attempt2BonusPins;
-                } else {
-                    runningTotal += nextFrame.pinsKnockedDownAttempt1();
-                }
+                runningTotal += getBonusScore(frames, frame, i);
             }
         }
         return runningTotal;
+    }
+
+    private int getBonusScore(List<Frame> frames, Frame currentFrame, int i){
+        Frame nextFrame = frames.get(i + 1);
+        int bonusScore;
+        if (currentFrame.pinsKnockedDownAttempt1() == 10) {
+            Frame nextNextFrame = frames.get(i + 2);
+            bonusScore = getBonusStrikeScore(nextFrame, nextNextFrame);
+        } else {
+            bonusScore = getBonusSpareScore(nextFrame);
+        }
+        return bonusScore;
+    }
+
+    private int getBonusSpareScore(Frame nextFrame) {
+        return nextFrame.pinsKnockedDownAttempt1();
+
+    }
+
+    private int getBonusStrikeScore(Frame nextFrame, Frame nextNextFrame) {
+        int attempt1BonusPins = nextFrame.pinsKnockedDownAttempt1();
+        int attempt2BonusPins;
+        if(attempt1BonusPins == 10){
+            attempt2BonusPins = nextNextFrame.pinsKnockedDownAttempt1();
+        } else {
+            attempt2BonusPins = nextFrame.pinsKnockedDownAttempt2();
+        }
+        return attempt1BonusPins + attempt2BonusPins;
     }
 }

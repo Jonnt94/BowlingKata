8e4d2d1c6c7d356a082bc78dcce11f42b82a310a
Jonnt94 <73747046+Jonnt94@users.noreply.github.com>
2023-08-06 21:11:05 +0100

    Adding FrameValidatorTests and logic.

--- src/main/java/kata/bowling/FrameValidator.java
+++ src/main/java/kata/bowling/FrameValidator.java
@@ -4,6 +4,52 @@ import java.util.List;
 
 public class FrameValidator {
     public boolean validate(List<Frame> frames){
+
+        if (frames.size() < 10 || frames.size() > 13){
+            return false;
+        }
+
+        if (hasAnyFrameScoreGreaterThan10(frames)) return false;
+
+        if (frames.get(9).isStrike()){
+            if(frames.size() == 10){
+                return false;
+            }
+            if(frames.size() == 11){
+                return true;
+            }
+            if (frames.size() == 12){
+                if(frames.get(10).isStrike()){
+                    if(frames.get(11).pinsKnockedDownAttempt2() == 0){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        if (frames.get(9).isSpare()){
+            if(frames.size() == 10){
+                return false;
+            }
+            if (frames.size() == 11){
+                if (frames.get(10).pinsKnockedDownAttempt2() == 0){
+                    return true;
+                }
+            }
+        }
+        if (frames.size() == 10){
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean hasAnyFrameScoreGreaterThan10(List<Frame> frames) {
+        for (Frame frame : frames){
+            if(frame.totalPinsKnockedDown() > 10){
+                return true;
+            }
+        }
         return false;
     }
 }
--- src/test/java/kata/bowling/FrameValidatorTest.java
+++ src/test/java/kata/bowling/FrameValidatorTest.java
@@ -3,22 +3,114 @@ package kata.bowling;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class FrameValidatorTest {
-
-//    ToDo: try and come up with other test scenarios. Strikes Spares and length of list. The length of the frame depends on if the was a strike/spare or not.
-//    ToDo: Need to check that frames don't add up to more ethan 10.
+    private static final List<Frame> NINE_STANDARD_FRAMES = List.of(Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1));
 
     @Test
     void returnsTrueWhenFramesListIsLengthOfTenAndTenthFrameIsNeitherStrikeNorSpare() {
-        List<Frame> frames = List.of(Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1),Frame.of(1,1));
+        List<Frame> frames = append(NINE_STANDARD_FRAMES,Frame.of(1,1));
 
         FrameValidator frameValidator = new FrameValidator();
         boolean valid = frameValidator.validate(frames);
 
         assertThat(valid).isTrue();
     }
+
+    @Test
+    void returnsFalseWhenFramesListLengthIsLessThanTen(){
+
+        FrameValidator frameValidator = new FrameValidator();
+        boolean valid = frameValidator.validate(NINE_STANDARD_FRAMES);
+
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void returnsTrueWhenFramesListIsLengthOfElevenIfTenthFrameIsStrike() {
+        List<Frame> frames = append(NINE_STANDARD_FRAMES,Frame.of(10,0),Frame.of(1,1));
+
+        FrameValidator frameValidator = new FrameValidator();
+        boolean valid = frameValidator.validate(frames);
+
+        assertThat(valid).isTrue();
+    }
+
+    @Test
+    void returnsTrueWhenFramesListIsLengthOfTwelfthIfTenthAndEleventhFramesAreBothStrikes() {
+        List<Frame> frames = append(NINE_STANDARD_FRAMES,Frame.of(10,0),Frame.of(10,0),Frame.of(1,0));
+
+        FrameValidator frameValidator = new FrameValidator();
+        boolean valid = frameValidator.validate(frames);
+
+        assertThat(valid).isTrue();
+    }
+
+    @Test
+    void returnsTrueWhenFramesListIsLengthOfElevenIfTenthFramesIsASpare(){
+        List<Frame> frames = append(NINE_STANDARD_FRAMES,Frame.of(5,5),Frame.of(1,0));
+
+        FrameValidator frameValidator = new FrameValidator();
+        boolean valid = frameValidator.validate(frames);
+
+        assertThat(valid).isTrue();
+    }
+
+    @Test
+    void returnsFalseWhenFramesAddUpToMoreThanTen(){
+        List<Frame> frames = append(NINE_STANDARD_FRAMES,Frame.of(6,6));
+
+        FrameValidator frameValidator = new FrameValidator();
+        boolean valid = frameValidator.validate(frames);
+
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void returnsFalseIfEleventhFrameHasTwoAttemptsWhenTenthFrameIsSpare(){
+        List<Frame> frames = append(NINE_STANDARD_FRAMES,Frame.of(5,5),Frame.of(1,1));
+
+        FrameValidator frameValidator = new FrameValidator();
+        boolean valid = frameValidator.validate(frames);
+
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void returnsFalseWhenTwelfthFrameHasTwoAttemptsWhenTenthAndEleventhFramesAreBothStrikes(){
+        List<Frame> frames = append(NINE_STANDARD_FRAMES,Frame.of(10,0),Frame.of(10,0),Frame.of(1,1));
+
+        FrameValidator frameValidator = new FrameValidator();
+        boolean valid = frameValidator.validate(frames);
+
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void returnsFalseWhenTenthFrameIsStrikeAndNoAdditionalFrames(){
+        List<Frame> frames = append(NINE_STANDARD_FRAMES,Frame.of(10,0));
+
+        FrameValidator frameValidator = new FrameValidator();
+        boolean valid = frameValidator.validate(frames);
+
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    void returnsFalseWhenTenthFrameIsSpareAndNoAdditionalFrames(){
+        List<Frame> frames = append(NINE_STANDARD_FRAMES,Frame.of(5,5));
+
+        FrameValidator frameValidator = new FrameValidator();
+        boolean valid = frameValidator.validate(frames);
+
+        assertThat(valid).isFalse();
+    }
+
+    private List<Frame> append(List<Frame> list, Frame... element){
+       return Stream.concat(list.stream(),Stream.of(element)).toList();
+    }
+
 }
